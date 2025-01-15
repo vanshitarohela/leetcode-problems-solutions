7f1891c3d692544b82af8fabee7663279e0d049c
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-01-15 18:24:33 +0530

    Time: 11 ms (74.86%) | Memory: 18.1 MB (20.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,11 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq = dict()
+        arr = [0] * 26
         for char in s:
-            freq[char] = freq.get(char, 0) + 1
+            arr[ord(char) - ord('a')] += 1
         for char in t:
-            freq[char] = freq.get(char, 0) - 1
-        for key in freq:
-            if freq.get(key, 0) != 0:
+            arr[ord(char) - ord('a')] -= 1
+        for i in arr:
+            if i != 0:
                 return False
         return True
+
+        # freq = dict()
+        # for char in s:
+        #     freq[char] = freq.get(char, 0) + 1
+        # for char in t:
+        #     freq[char] = freq.get(char, 0) - 1
+        # for key in freq:
+        #     if freq.get(key, 0) != 0:
+        #         return False
+        # return True
+        # ******** 11
+        # ms
+        # Beats
+        # 74.86%
+
+
