40c5695c611d825c3b8a6220b9d05298b41c84f0
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-01-18 18:41:09 +0530

    Time: 12 ms (61.46%) | Memory: 20.7 MB (62.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,8 +6,9 @@ class Solution:
             string = ''.join(sorted(orig))
             if string not in visit:
                 visit[string] = [orig]
+                res.append(visit[string])
             else:
                 visit[string].append(orig)
-        for key in visit:
-            res.append(visit[key])
+        # for key in visit:
+        #     res.append(visit[key])
         return res
