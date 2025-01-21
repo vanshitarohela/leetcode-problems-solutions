80f845f75a20c5d152823aa56e417fcbde707b3c
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-01-22 01:21:01 +0530

    Time: 59 ms (48.52%) | Memory: 34.4 MB (18.17%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,17 +3,25 @@ class Solution:
         s = set(nums)
         max_seq = 0
 
-        while len(s) != 0:
-            for i in s:
-                x = y = i
-                s.remove(i)
-                break
-            while x - 1 in s:
-                x = x - 1
-                s.remove(x)
-            while y + 1 in s:
-                y = y + 1
-                s.remove(y)
-            max_seq = max(max_seq, y-x+1)
+        for i in s:
+            if i-1 not in s:
+                seq = 0
+                while i + seq in s:
+                    seq += 1
+                max_seq = max(max_seq, seq)
         return max_seq
-            
+
+
+        # while len(s) != 0:
+        #     for i in s:
+        #         x = y = i
+        #         s.remove(i)
+        #         break
+        #     while x - 1 in s:
+        #         x = x - 1
+        #         s.remove(x)
+        #     while y + 1 in s:
+        #         y = y + 1
+        #         s.remove(y)
+        #     max_seq = max(max_seq, y-x+1)
+        # return max_seq            
