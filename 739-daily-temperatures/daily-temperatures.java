ce423a015d3547d583342804adbcf3f131e51046
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-06-01 01:15:51 +0530

    Time: 92 ms (5.31%) | Memory: 63.2 MB (19.63%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -2,18 +2,29 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] ans = new int[temperatures.length];
         Stack<Integer> s = new Stack<>();
-        for(int i = temperatures.length-1; i>=0; i--) {
-            while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
-                s.pop();
-            }
+        for(int i = 0; i < temperatures.length; i++) {
             if (!s.isEmpty()) {
-                ans[i] = s.peek() - i;
-                s.push(i);
-            } else {
-                ans[i] = 0;
-                s.push(i);
+                while (!s.isEmpty() && temperatures[s.peek()] < temperatures[i]) {
+                    ans[s.peek()] = i-s.peek();
+                    s.pop();
+                }
             }
+            s.push(i);
         }
+
+        // for(int i = temperatures.length-1; i>=0; i--) {
+        //     while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+        //         s.pop();
+        //     }
+        //     if (!s.isEmpty()) {
+        //         ans[i] = s.peek() - i;
+        //         s.push(i);
+        //     } else {
+        //         ans[i] = 0;
+        //         s.push(i);
+        //     }
+        // }
+
     // for(int i = temperatures.length-1; i>=0; i--) {
     //     if (s.isEmpty()) {
     //         ans[i] = 0;
