771feee9d4e4cbe39e4b8896145f770d89332ee7
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-06-01 00:44:03 +0530

    Time: 81 ms (20.61%) | Memory: 60.2 MB (70.41%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -3,27 +3,38 @@ class Solution {
         int[] ans = new int[temperatures.length];
         Stack<Integer> s = new Stack<>();
         for(int i = temperatures.length-1; i>=0; i--) {
-            if (s.isEmpty()) {
-                ans[i] = 0;
+            while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+                s.pop();
+            }
+            if (!s.isEmpty()) {
+                ans[i] = s.peek() - i;
                 s.push(i);
             } else {
-                if (temperatures[s.peek()] > temperatures[i]) {
-                    ans[i] = s.peek()-i;
-                    s.push(i);
-                } else {
-                    while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
-                        s.pop();
-                    }
-                    if (!s.isEmpty()) {
-                        ans[i] = s.peek() - i;
-                        s.push(i);
-                    } else {
-                        ans[i] = 0;
-                        s.push(i);
-                    }
-                }
+                ans[i] = 0;
+                s.push(i);
             }
         }
-        return ans;
-    }
-}
+    // for(int i = temperatures.length-1; i>=0; i--) {
+    //     if (s.isEmpty()) {
+    //         ans[i] = 0;
+    //         s.push(i);
+    //     } else {
+    //         if (temperatures[s.peek()] > temperatures[i]) {
+    //             ans[i] = s.peek()-i;
+    //             s.push(i);
+    //         } else {
+    //             while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+    //                 s.pop();
+    //             }
+    //             if (!s.isEmpty()) {
+    //                 ans[i] = s.peek() - i;
+    //                 s.push(i);
+    //             } else {
+    //                 ans[i] = 0;
+    //                 s.push(i);
+    //             }
+    //         }
+    //     }
+    // }
+    return ans;
+}}
