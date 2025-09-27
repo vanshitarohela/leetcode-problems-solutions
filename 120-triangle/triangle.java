e3bdcc75837b91f5d3b6146cf059eda836eaa31b
Vanshita Rohela <73214421+vanshitarohela@users.noreply.github.com>
2025-09-28 00:04:59 +0530

    Time: 12 ms (5.00%) | Memory: 46.2 MB (8.38%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,23 +1,22 @@
 // import java.Math.*;
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        // int total = triangle.get(0).get(0);
         int ans = triangle.get(0).get(0);
         for(int i = 1; i < triangle.size(); i++) {
-            int rowMin = triangle.get(i).get(0) + triangle.get(i-1).get(0);
+            ans = Integer.MAX_VALUE;
             for(int j = 0; j < i+1; j++) {
                 if(j == 0) {
                     triangle.get(i).set(j, triangle.get(i-1).get(j) + triangle.get(i).get(j));
                 } else if (j == i) {
                     triangle.get(i).set(j, triangle.get(i-1).get(j-1) + triangle.get(i).get(j));
-                    rowMin = Math.min(rowMin, triangle.get(i).get(j));
+                    // ans = Math.min(ans, triangle.get(i).get(j));
                 } else {
                     triangle.get(i).set(j, Math.min(triangle.get(i-1).get(j-1) + triangle.get(i).get(j), triangle.get(i-1).get(j) + triangle.get(i).get(j)));
-                    rowMin = Math.min(rowMin, triangle.get(i).get(j));
                 }
+                ans = Math.min(ans, triangle.get(i).get(j));
             }
 
-            ans = rowMin;
+            // ans = ans;
         }
 
         return ans;
